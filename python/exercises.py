2e401923ae76d6206573426f389f6d34aac4bb9a
Vahan Gevorgyan <47473115+vahan-gev@users.noreply.github.com>
2024-09-09 13:54:10 -0700

    Python section - done

--- python/exercises.py
+++ python/exercises.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from collections.abc import Callable
-
+from typing import Optional, Iterator
 
 def change(amount: int) -> dict[int, int]:
     if not isinstance(amount, int):
@@ -14,15 +14,96 @@ def change(amount: int) -> dict[int, int]:
 
 
 # Write your first then lower case function here
-
+def first_then_lower_case(a: list[str], p: Callable[[str], bool]) -> Optional[str]:
+    for i in a:
+        if p(i):
+            return i.lower()
+    return None
 
 # Write your powers generator here
+def powers_generator(base:int, limit:int) -> Iterator[list[int]]:
+    arr = []
+    index = 0;
+    while base**index <= limit:
+        arr.append(base**index)
+        index += 1
+    return iter(arr)
 
 
 # Write your say function here
+# Used answer by: https://stackoverflow.com/users/4952130/dimitris-fasarakis-hilliard at https://stackoverflow.com/questions/39038358/function-chaining-in-python
+class CustomString(str):
+    def __call__(self, value=None):
+        if value == None:
+            return CustomString(self + "")
+        return CustomString(self + " " + value)
 
+def say(text: str="") -> str:
+    return CustomString(text)
 
 # Write your line count function here
-
+def meaningful_line_count(file_path: str) -> int:
+    count = 0
+    try:
+        file = open(file_path, "r")
+        for line in file:
+            if len(line) > 0 and not line.isspace():
+                for char in line:
+                    if not char.isspace():
+                        if char != "#":
+                            count += 1
+                            break
+                        else:
+                            break
+        file.close()
+    except:
+        raise FileNotFoundError("No such file")
+    return count
 
 # Write your Quaternion class here
+@dataclass(frozen=True)
+class Quaternion:
+    a: float
+    b: float
+    c: float
+    d: float
+
+    def __str__(self):
+        result = ""
+        if self.a != 0 or (self.b == 0 and self.c == 0 and self.d == 0):
+            result += str(self.a)
+        if self.b != 0:
+            result += "+" if (self.b > 0 and result != "") else ""
+            result += ("" if self.b == 1 else "-" if self.b == -1 else str(self.b)) + "i"
+        if self.c != 0:
+            result += "+" if (self.c > 0 and result != "") else ""
+            result += ("" if self.c == 1 else "-" if self.c == -1 else str(self.c)) + "j"
+        if self.d != 0:
+            result += "+" if (self.d > 0 and result != "") else ""
+            result += ("" if self.d == 1 else "-" if self.d == -1 else str(self.d)) + "k"
+        return result or "0"
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        if isinstance(other, Quaternion):
+            return Quaternion(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
+        return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, Quaternion):
+            return Quaternion(
+                self.a * other.a - self.b * other.b - self.c * other.c - self.d * other.d,
+                self.a * other.b + self.b * other.a + self.c * other.d - self.d * other.c,
+                self.a * other.c - self.b * other.d + self.c * other.a + self.d * other.b,
+                self.a * other.d + self.b * other.c - self.c * other.b + self.d * other.a
+            )
+        return NotImplemented
+
+    @property
+    def conjugate(self):
+        return Quaternion(self.a, -self.b, -self.c, -self.d)
+
+    @property
+    def coefficients(self):
+        return (self.a, self.b, self.c, self.d)
